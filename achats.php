702a6fb51dabd18b1a3098dee8321b5bc2f4d2fa
QuantumSoul <QuantumSoul@caramail.fr>
2016-04-03 17:10:54 +0200

    Pimpage de la navbar

--- achats.php
+++ achats.php
@@ -1,3 +1,29 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="css/custom.css">
+        <style>
+            .criteres {
+                background-color: rgba ();
+            }
+        </style>
+    </head>
+    
+    <body>
+        <?php include 'navbar.php'; ?>
+        
+        <div class="container">
+            <div class="col-lg-6 col-lg-offset-3">
+                
+            </div>
+        </div>
+        
+    </body>
+</html>
--- navbar.php
+++ navbar.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-fixed-top menu">
     <div class="container-fluid">
-        <ul class="nav navbar-nav">
-            <li><a class="btn menu-button" href="/Projet_Web_Brony">Accueil</a></li>
+        <ul id="nav" class="nav navbar-nav">
+            <li><a id="menu1" class="btn" href="/Projet_Web_Brony">Accueil</a></li>
             <li class="dropdown">
-                <a class="btn menu-button">Achats</a>
+                <a id="menu2" class="btn menu-button">Achats</a>
                 <div class="dropdown-content">
                     <a href="#">Poneys</a>
                     <a href="#">Licornes</a>
@@ -12,17 +12,16 @@
                 </div>
             </li>
             <li class="dropdown">
-                <a class="btn menu-button">Ventes</a>
+                <a id="menu3" class="btn menu-button">Ventes</a>
                 <div class="dropdown-content">
                     <a href="#">Revendre</a>
                     <a href="#">Clauses revendeur</a>
                 </div>
             </li>
-            <li><a class="btn menu-button">FAQ</a></li>
-            <li><a class="btn menu-button">Contact</a></li>
+            <li><a id="menu4" class="btn menu-button">Contact</a></li>
             <li class="dropdown">
-                <a class="btn menu-button">Mon Compte</a>
-                <div class="dropdown-content">
+                <a id="menu5" class="btn menu-button">Mon Compte</a>
+                <div class="dropdown-content" style="background: linear-gradient(to right, #551a82, #0090d0, #3cb240, #ffee55, #fe7e30, #eb0021)">
                     <?php
 
                         if ($_SESSION['users']['login']!='')
@@ -39,9 +38,9 @@
                         else
                         {
                             echo '<form action="index.php" method="POST">
-                                    Identifiant
+                                    <span style="color: whitesmoke">Identifiant
                                     <br><input type="text" class="form-control" name="identifiant" />
-                                    Mot de passe
+                                    Mot de passe</span>
                                     <br><input type="password" class="form-control" name="mdp" />
                                     <button type="submit" class="btn btn-default" style="min-width: 100%">Connexion</button>
                                   </form>
@@ -51,7 +50,7 @@
                 </div>
             </li>
             <li class="dropdown">
-                <a class="btn menu-button">À propos</a>
+                <a id="menu6" class="btn menu-button">À propos</a>
                 <div class="dropdown-content">
                     <a href="#">Le site</a>
                     <a href="#">La team</a>
